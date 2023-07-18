37f733be665c0d26c409a813bce1e0a071f35d33
root <abdvswmdr@gmail.com>
2023-07-18 03:38:51 -0700

    hey

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 print("First 3 letters: {}".format(word[: 3]))
-print("Last 2 letters: {}".format(word[6: ]))
-print("Middle word: {}".format(word[1:9]))
+print("Last 2 letters: {}".format(word[7: ]))
+print("Middle word: {}".format(word[1:8]))
 #Commenting
 #Commenting
 #COMENET
