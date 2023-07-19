1df514bca9d67a4f2d6a29d6dd78fb151306466a
root <abdvswmdr@gmail.com>
2023-07-18 21:38:31 -0700

    hello

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,7 +8,7 @@ if number < 0 and last_digit < 5 and last_digit > 0:
 elif number < 0 and last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif number > 0 and last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and s 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 elif number > 0 and last_digit <= 5 and last_digit > 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
 elif number < 0 and last_digit >= 5:
