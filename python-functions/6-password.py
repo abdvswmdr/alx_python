c18681d96eb8360ea773e1b0484e66b21d715186
root <abdvswmdr@gmail.com>
2023-07-21 13:08:54 -0700

    he

---
+++ python-functions/6-password.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+def validate_password(password):
+    # Check password length
+    if len(password) < 8:
+        return False
+
+    #Check for uppercase, lowercase and a digit atleast
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+
+
+    if not (has_uppercase and has_lowercase and has_digit):
+        return False
+
+
+    # Check for spaces in password
+    if ' ' in password:
+        return False
+
+    return True
+
